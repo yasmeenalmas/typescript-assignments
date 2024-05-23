05e4b5a473b18e8235a91c26b6228e71e76c87ac
yasmeenalmas <165240821+yasmeenalmas@users.noreply.github.com>
2024-05-22 21:39:06 -0700

    Add files via upload

---
+++ Array Assigment/array.ts
@@ -0,0 +1,70 @@
+//  //// Question 01 /////
+
+// const fruits : string[] = ["apple", "banana", "mango", "orange"];
+// console.log("fruits", fruits);
+
+//  ///// Question 02 /////
+
+//  const numbers : number[] = [10 , 20 , 30 , 40];
+//  console.log("numbers" , numbers);
+
+//  //// Question 03 ////
+
+//  const fruits : string[] = ["apple", "banana", "mango", "orange"];
+//  let thirdfruit = fruits[2]
+//  console.log("fruits", fruits);
+//  console.log("thirdfruit" , thirdfruit);
+
+//  ///// Question 04 /////
+
+//  const numbers : number[] = [10 , 20 ,30 ,40 ,];
+//  numbers[1] = 25;
+//  console.log("numbers", numbers);
+
+// //  ///// Question 05 /////
+
+//  const fruits : string[] =["apple", "banana","mango", "orange"];
+//  fruits.push("grapes");
+//  console.log("fruits", fruits);
+
+// ///// Question 06 /////
+
+// const fruits : string[] = ["apple" , "banana", "mango", "orange"];
+// let lastfruit = fruits.pop();
+// console.log("fruits", fruits);
+
+// ///// Question 07 /////
+
+// const fruits : string[] =["apple","banana","mango","orange"];
+// let firstfruit = fruits.shift();
+// console.log("fruits", fruits);
+
+// ///// Question 08 //////
+
+// const fruits : string[] =["apple", "banana", "mango", "orange"];
+// fruits.unshift("kiwi");
+// console.log("fruits", fruits);
+
+// ///// Question 09 //////
+
+// const fruits : string[] =["apple", "banana","mango","orange"];
+// fruits.splice(1,2);
+// console.log("fruits", fruits);
+
+// ////// Qyestion 10 ////// 
+
+// const fruits : string[] =["apple", "banana", "mango","orange"];
+// fruits.splice(1,2, "pineapple", "pear");
+// console.log("fruits", fruits);
+
+// ///// Question 11 /////
+
+// const fruits : string[] =["apple", "banana","mango","orange"];
+// let citrusfruits = fruits.splice(0,2);
+// console.log("citrusfruits", citrusfruits);
+
+// ////// Question 12 //////
+
+// const fruits : string[] =["apple","banana","mango","orange"];
+// let lastTwofruits = fruits.splice(2,3);
+// console.log("lastTwofruits", lastTwofruits);
